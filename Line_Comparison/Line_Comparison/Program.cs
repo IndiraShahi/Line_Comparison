faf789370cfa0a231b750cef69160f23824c90dc
IndiraShahi <IndiraShahi144@gmail.com>
2021-06-01 02:27:31 +0530

    Equality of two Lines

--- Line_Comparison/Line_Comparison/Program.cs
+++ Line_Comparison/Line_Comparison/Program.cs
@@ -9,7 +9,14 @@ namespace Line_Comparison
         {
             Console.WriteLine("Welcome to Line Computation");
             Line line1 = new Line();
-            double Line = line1.LengthOfLine(0, 0, 2, 2);
+            double Line1 = line1.LengthOfLine(0, 0, 2, 2);
+            Line line2 = new Line();
+            double Line2 = line2.LengthOfLine(0, 0, 4, 4);
+            //using equals
+            if (line1.Equals(line2))
+                Console.WriteLine("lines are equal");
+            else
+                Console.WriteLine("lines are not equal");
         }
     }
 }
