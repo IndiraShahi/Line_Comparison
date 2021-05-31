999e6c648ebf09351969d363a3f545bd3f5ec088
IndiraShahi <IndiraShahi144@gmail.com>
2021-06-01 02:18:08 +0530

    Calculate length of line

---
+++ Line_Comparison/Line_Comparison/Line.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Line_Comparison
+{
+    internal class Line
+    {
+        internal double LengthOfLine(int x1, int y1, int x2, int y2)
+        {
+
+            double LengthOfLine = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine(LengthOfLine);
+            return LengthOfLine;
+
+        }
+    }
+}
--- Line_Comparison/Line_Comparison/Program.cs
+++ Line_Comparison/Line_Comparison/Program.cs
@@ -4,9 +4,12 @@ namespace Line_Comparison
 {
     class Program
     {
+       
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Line Computation");
+            Line line1 = new Line();
+            double Line = line1.LengthOfLine(0, 0, 2, 2);
         }
     }
 }
