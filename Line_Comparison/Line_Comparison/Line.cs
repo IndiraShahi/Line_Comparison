f1bcad121629c2f8a446fd4db33c3a7c267d0935
IndiraShahi <IndiraShahi144@gmail.com>
2021-06-03 21:34:19 +0530

    [Indira]:ADDED:Using abstract class

--- Line_Comparison/Line_Comparison/Line.cs
+++ Line_Comparison/Line_Comparison/Line.cs
@@ -2,14 +2,17 @@
 
 namespace Line_Comparison
 {
-    class Line
-    {
-
-        public double LengthOfLine(int x1, int y1, int x2, int y2)
+        public abstract class Line
         {
-
-            double LengthOfLine = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-            Console.WriteLine(LengthOfLine);
+            public abstract double LengthOfLine(int x1, int y1, int x2, int y2);
+        }
+        public class Length:Line
+        {
+            public override double LengthOfLine(int x1, int y1, int x2, int y2)
+        {
+            double LengthOfLine = 0;
+            LengthOfLine = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("Length Of Line is=" +LengthOfLine);
             return LengthOfLine;
 
         }
--- Line_Comparison/Line_Comparison/Program.cs
+++ Line_Comparison/Line_Comparison/Program.cs
@@ -7,26 +7,26 @@ namespace Line_Comparison
         static void Main()
         {
             Console.WriteLine("Welcome to Line Comparison Computation Program");
-            Line line1 = new Line();
-            Line line2 = new Line();
-            double FirstLine = line1.LengthOfLine(0, 0, 2, 2);
-            double SecondLine = line2.LengthOfLine(0, 0, 4, 4);
+            Length Line;
+            Line = new Length();
+            double FirstLine = Line.LengthOfLine(0, 0, 2, 2);
+            double SecondLine = Line.LengthOfLine(2, 2, 0, 0);
 
             //using Equals
             if (FirstLine.Equals(SecondLine))
-                Console.WriteLine("Lines are equal");
+                Console.WriteLine("Using Equlas : Lines are equal");
             else
-                Console.WriteLine("Lines are not equal");
+                Console.WriteLine("Using Equlas : Lines are not equal");
 
             //using compareTo
             double comparison = (FirstLine.CompareTo(SecondLine));
             if (comparison == 0)
             {
-                Console.WriteLine("Lines are same length");
+                Console.WriteLine("Using compareTo : Lines are of same length");
             }
             else
             {
-                Console.WriteLine("Lines are not same length");
+                Console.WriteLine("Using compareTo : Lines are not of same length");
             }
         }
     }
